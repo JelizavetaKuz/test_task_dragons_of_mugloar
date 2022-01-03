e1cb67ca3e59d8858f0ee877d258f2323b5beb19
Jelizaveta Kuznetsova <jelizavet@ut.ee>
2022-01-03 05:35:24 +0200

    Last touches

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img class="w-75" src="https://dragonsofmugloar.com/assets/logo.png"/>
+    <img class="w-75" src="https://dragonsofmugloar.com/assets/logo.png" alt="Dragons of Mugloar"/>
     <br>
     <button type="button" class="btn btn-primary start" style="color: var(--yellow-money); background-color: var(--black-red)"  @click="go_to_menu()">Start Adventure</button>
   </div>
